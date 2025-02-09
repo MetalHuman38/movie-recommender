e7c8c6eefbe47bfc128df56df8b23e6eeadb654b
MetalHuman38 <kalejaib@roehampton.ac.uk>
2025-02-09 02:01:33 +0000

    qstash-workflows

--- lib/corsMiddleware.ts
+++ lib/corsMiddleware.ts
@@ -1,15 +1,42 @@
-import Cors from "next-cors";
-import { NextRequest, NextResponse } from "next/server";
+import { NextResponse } from "next/server";
 
-export async function corsMiddleware(req: NextRequest, res: NextResponse) {
-  await Cors(req, res);
-  res.headers.set(
+export async function GET(req: Request) {
+  const response = NextResponse.json({
+    message: "CORS enabled for Next.js API",
+  });
+
+  // Set CORS headers manually
+  response.headers.set(
     "Access-Control-Allow-Origin",
     "https://movie-recommender-fo9ndjxvh-babatunde-kalejaiyes-projects.vercel.app"
   );
-  res.headers.set(
+  response.headers.set(
     "Access-Control-Allow-Methods",
-    "GET,POST,PUT,DELETE,OPTIONS"
+    "GET, POST, PUT, DELETE, OPTIONS"
   );
-  res.headers.set("Access-Control-Allow-Headers", "Content-Type,Authorization");
+  response.headers.set(
+    "Access-Control-Allow-Headers",
+    "Content-Type, Authorization"
+  );
+
+  return response;
+}
+
+export async function OPTIONS() {
+  const response = new NextResponse(null, { status: 200 });
+
+  response.headers.set(
+    "Access-Control-Allow-Origin",
+    "https://movie-recommender-fo9ndjxvh-babatunde-kalejaiyes-projects.vercel.app"
+  );
+  response.headers.set(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PUT, DELETE, OPTIONS"
+  );
+  response.headers.set(
+    "Access-Control-Allow-Headers",
+    "Content-Type, Authorization"
+  );
+
+  return response;
 }
